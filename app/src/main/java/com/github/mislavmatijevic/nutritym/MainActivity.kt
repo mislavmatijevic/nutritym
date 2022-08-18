6b17e461930adbce713f23665904fedb25355e41
Mislav Matijević <mislav.mim@gmail.com>
2022-08-18 09:27:51 +0200

    Created new "Empty Activity" project.

---
+++ app/src/main/java/com/github/mislavmatijevic/nutritym/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.mislavmatijevic.nutritym
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
