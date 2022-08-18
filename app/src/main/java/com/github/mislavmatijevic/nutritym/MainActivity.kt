b254737aa3903731b00d8131c0d65cb1be63e3d4
Mislav Matijević <mislav.mim@gmail.com>
2022-08-18 10:12:05 +0200

    View binding used in MainActivity.

--- app/src/main/java/com/github/mislavmatijevic/nutritym/MainActivity.kt
+++ app/src/main/java/com/github/mislavmatijevic/nutritym/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.github.mislavmatijevic.nutritym
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.mislavmatijevic.nutritym.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
