37360d5f3d0f235c4509fe2614deea319e2f0006
Mislav Matijević <mislav.mim@gmail.com>
2022-08-24 20:51:28 +0200

    Prepared Photo "database" class.

---
+++ database/src/main/java/com/github/mislavmatijevic/nutritym/database/NutritymDatabase.kt
@@ -0,0 +1,30 @@
+package com.github.mislavmatijevic.nutritym.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.github.mislavmatijevic.nutritym.database.dao.PhotoDao
+import com.github.mislavmatijevic.nutritym.database.entity.Photo
+
+@Database(entities = [Photo::class], version = 1, exportSchema = false)
+abstract class NutritymDatabase : RoomDatabase() {
+
+    abstract fun getPhotoDao(): PhotoDao
+
+    companion object {
+        @Volatile
+        private var instance: NutritymDatabase? = null
+
+        fun getDatabase(context: Context): NutritymDatabase? {
+            return instance ?: synchronized(this) {
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NutritymDatabase::class.java,
+                    "nutrity_database"
+                ).build()
+                return instance
+            }
+        }
+    }
+}
